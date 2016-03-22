2da33540782ce6edec4ade97fbbe1a88496ff8d9
Oliver Gerler <rockus@rockus.at>
2016-03-22 01:37:45 +0100

    add pressure

--- bme280/gatherAndSend.c
+++ bme280/gatherAndSend.c
@@ -93,11 +93,9 @@ static int read_bme280_dat(int fd, struct data *data)
 int compensate_data(int fd, struct data *data)
 {
   int address, byte;
+
   unsigned short dig_T1, dig_T2, dig_T3;
   unsigned char dig_H1, dig_H3;
-  signed short dig_H2, dig_H4, dig_H5;
-  signed char dig_H6;
-
   // dig_T1
   address = 0x89; // dig_T1[15:8]
   byte = wiringPiI2CReadReg8(fd, address);
@@ -147,12 +145,17 @@ int compensate_data(int fd, struct data *data)
   ivar1 = ((((data->temp_raw>>3) - ((int32_t)dig_T1<<1))) * ((int32_t)dig_T2)) >> 11;
   ivar2 = (((((data->temp_raw>>4) - ((int32_t)dig_T1)) * ((data->temp_raw>>4) - ((int32_t)dig_T1))) >> 12) * ((int32_t)dig_T3)) >> 14;
   it_fine = ivar1 + ivar2;
+
   iT = (it_fine * 5 + 128) >> 8;
   printf ("ivar1: %ld\n", ivar1);
   printf ("ivar2: %ld\n", ivar2);
   printf("it_fine: %ld\n", it_fine);
   printf ("iT: %5.2fC\n", data->Temperature);
 */
+
+
+  signed short dig_H2, dig_H4, dig_H5;
+  signed char dig_H6;
 
   // dig_H1
   address = 0xa1; // dig_H1[7:0]
@@ -216,20 +219,168 @@ int compensate_data(int fd, struct data *data)
 //  printf ("H: %5.2f%%\n", H);
 
 
-    printf("Humidity:%.2f%% Temperature:%.2f°C Pressure:%.2fPa\n", H, T, 0.0 );
-    syslog(LOG_INFO, "Humidity:%.2f%% Temperature:%.2f°C Pressure:%.2fPa\n", H, T, 0.0 );
+  unsigned short dig_P1;
+  signed short dig_P2, dig_P3, dig_P4, dig_P5, dig_P6, dig_P7, dig_P8, dig_P9;
 
-	data->Humidity = H;
-	data->Temperature = T;
+  // dig_P1
+  address = 0x8f; // dig_P1[15:8]
+  byte = wiringPiI2CReadReg8(fd, address);
+//  printf ("%02x", byte);
+  dig_P1 = (unsigned short)(byte<<8);
+  address = 0x8e; // dig_P1[7:0]
+  byte = wiringPiI2CReadReg8(fd, address);
+//  printf ("%02x ", byte);
+  dig_P1 |= (unsigned short)(byte);
+//  printf ("dig_P1: 0x%04x %d\n", dig_P1, dig_P1);
 
-	// simple plausibility check
-	if (T < -100.0)
-	{
-		syslog(LOG_INFO, "error: temp < -100.0.");
-		return 0;
-	}
+  // dig_P2
+  address = 0x91; // dig_P2[15:8]
+  byte = wiringPiI2CReadReg8(fd, address);
+//  printf ("%02x", byte);
+  dig_P2 = (signed short)(byte<<8);
+  address = 0x90; // dig_P2[7:0]
+  byte = wiringPiI2CReadReg8(fd, address);
+//  printf ("%02x ", byte);
+  dig_P2 |= (signed short)(byte);
+//  printf ("dig_P2: 0x%04x %d\n", dig_P2, dig_P2);
 
-    return 1;
+  // dig_P3
+  address = 0x93; // dig_P3[15:8]
+  byte = wiringPiI2CReadReg8(fd, address);
+//  printf ("%02x", byte);
+  dig_P3 = (signed short)(byte<<8);
+  address = 0x92; // dig_H5[7:0]
+  byte = wiringPiI2CReadReg8(fd, address);
+//  printf ("%02x ", byte);
+  dig_P3 |= (signed short)(byte);
+//  printf ("dig_P3: 0x%04x %d\n", dig_P3, dig_P3);
+
+  // dig_P4
+  address = 0x95; // dig_P4[15:8]
+  byte = wiringPiI2CReadReg8(fd, address);
+//  printf ("%02x", byte);
+  dig_P4 = (signed short)(byte<<8);
+  address = 0x94; // dig_P4[7:0]
+  byte = wiringPiI2CReadReg8(fd, address);
+//  printf ("%02x ", byte);
+  dig_P4 |= (signed short)(byte);
+//  printf ("dig_P4: 0x%04x %d\n", dig_P4, dig_P4);
+
+  // dig_P5
+  address = 0x97; // dig_P5[15:8]
+  byte = wiringPiI2CReadReg8(fd, address);
+//  printf ("%02x", byte);
+  dig_P5 = (signed short)(byte<<8);
+  address = 0x96; // dig_P5[7:0]
+  byte = wiringPiI2CReadReg8(fd, address);
+//  printf ("%02x ", byte);
+  dig_P5 |= (signed short)(byte);
+//  printf ("dig_P5: 0x%04x %d\n", dig_P5, dig_P5);
+
+  // dig_P6
+  address = 0x99; // dig_P6[15:8]
+  byte = wiringPiI2CReadReg8(fd, address);
+//  printf ("%02x", byte);
+  dig_P6 = (signed short)(byte<<8);
+  address = 0x98; // dig_P6[7:0]
+  byte = wiringPiI2CReadReg8(fd, address);
+//  printf ("%02x ", byte);
+  dig_P6 |= (signed short)(byte);
+//  printf ("dig_P6: 0x%04x %d\n", dig_P6, dig_P6);
+
+  // dig_P7
+  address = 0x9b; // dig_P7[15:8]
+  byte = wiringPiI2CReadReg8(fd, address);
+//  printf ("%02x", byte);
+  dig_P7 = (signed short)(byte<<8);
+  address = 0x9a; // dig_P7[7:0]
+  byte = wiringPiI2CReadReg8(fd, address);
+//  printf ("%02x ", byte);
+  dig_P7 |= (signed short)(byte);
+//  printf ("dig_P7: 0x%04x %d\n", dig_P7, dig_P7);
+
+  // dig_P8
+  address = 0x9d; // dig_P8[15:8]
+  byte = wiringPiI2CReadReg8(fd, address);
+//  printf ("%02x", byte);
+  dig_P8 = (signed short)(byte<<8);
+  address = 0x9c; // dig_P8[7:0]
+  byte = wiringPiI2CReadReg8(fd, address);
+//  printf ("%02x ", byte);
+  dig_P8 |= (signed short)(byte);
+//  printf ("dig_P8: 0x%04x %d\n", dig_P8, dig_P8);
+
+  // dig_P9
+  address = 0x9f; // dig_P9[15:8]
+  byte = wiringPiI2CReadReg8(fd, address);
+//  printf ("%02x", byte);
+  dig_P9 = (signed short)(byte<<8);
+  address = 0x9e; // dig_P9[7:0]
+  byte = wiringPiI2CReadReg8(fd, address);
+//  printf ("%02x ", byte);
+  dig_P9 |= (signed short)(byte);
+//  printf ("dig_P9: 0x%04x %d\n", dig_P9, dig_P9);
+
+  // compensate press
+  double P;
+  var1 = ((double)t_fine/2.0) - 64000.0;
+  var2 = var1 * var1 * ((double)dig_P6) / 32768.0;
+  var2 = var2 + var1 * ((double)dig_P5) * 2.0;
+  var2 = (var2/4.0)+(((double)dig_P4) * 65536.0);
+  var1 = (((double)dig_P3) * var1 * var1 / 524288.0 + ((double)dig_P2) * var1) / 524288.0;
+  var1 = (1.0 + var1 / 32768.0)*((double)dig_P1);
+  if (var1 != 0.0)	// check for possible division by zero
+  {
+    P = 1048576.0 - ((double)data->press_raw);
+    P = (P - (var2 / 4096.0)) * 6250.0 / var1;
+    var1 = ((double)dig_P9) * P * P / 2147483648.0;
+    var2 = P * ((double)dig_P8) / 32768.0;
+    P = P + (var1 + var2 + ((double)dig_P7)) / 16.0;
+  }
+  else
+  {
+    P = 0.0;
+  }
+
+/*
+  long unsigned int iP;
+  ivar1 = (((int32_t)it_fine)>>1) - (int32_t)64000;
+  ivar2 = (((ivar1>>2) * (ivar1>>2)) >> 11 ) * ((int32_t)dig_P6);
+  ivar2 = ivar2 + ((ivar1*((int32_t)dig_P5))<<1);
+  ivar2 = (ivar2>>2)+(((int32_t)dig_P4)<<16);
+  ivar1 = (((((int32_t)dig_P3) * (((ivar1>>2) * (ivar1>>2)) >> 13 )) >> 3) + ((((int32_t)dig_P2) * ivar1)>>1))>>18;
+  ivar1 = ((((32768+ivar1))*((int32_t)dig_P1))>>15);
+  // avoid divide-by-zero here
+  iP = (((uint32_t)(((int32_t)1048576)-((int32_t)data->press_raw))-(ivar2>>12)))*3125;
+  if (iP < 0x80000000)
+  {
+    iP = (iP << 1) / ((uint32_t)ivar1);
+  }
+  else
+  {
+    iP = (iP / ((uint32_t)ivar1)) * 2.0;
+  }
+  ivar1 = (((int32_t)dig_P9) * ((int32_t)(((iP>>3) * (iP>>3))>>13)))>>12;
+  ivar2 = (((int32_t)(iP>>2)) * ((int32_t)dig_P8))>>13;
+  iP = (uint32_t)((int32_t)iP + ((ivar1 + ivar2 + dig_P7) >> 4));
+  printf ("iP: %lu\n", iP);
+*/
+
+  printf("Humidity:%.2f%% Temperature:%.2f°C Pressure:%.2fhPa\n", H, T, P/100.0 );
+  syslog(LOG_INFO, "Humidity:%.2f%% Temperature:%.2f°C Pressure:%.2fhPa\n", H, T, P/100.0 );
+
+  data->Humidity = H;
+  data->Temperature = T;
+  data->Pressure = P;
+
+  // simple plausibility check
+  if (T < -100.0)
+  {
+    syslog(LOG_INFO, "error: temp < -100.0.");
+    return 0;
+  }
+
+  return 1;
 }
 
 int gatherData(struct config *config, struct data *data)
@@ -264,7 +415,7 @@ int sendToEmonCMS (struct config *config, struct data *data, int socket_fd)
 //    printf ("socket_fd: %d\n", socket_fd);
 
     // generate json string for emonCMS
-    sprintf (tcp_buffer, "GET /input/post.json?node=\"%s-env\"&json={Humidity-BME280:%4.2f,Temperature-BME280:%4.2f}&apikey=%s HTTP/1.1\r\nHost: %s\r\nUser-Agent: %s %s\r\nConnection: keep-alive\r\n\r\n", config->pNodeName, data->Humidity, data->Temperature, config->pApiKey, config->pHostName, TOOLNAME, BME280_VERSION);
+    sprintf (tcp_buffer, "GET /input/post.json?node=\"%s-env\"&json={Humidity-BME280:%4.2f,Temperature-BME280:%4.2f,Pressure-BME280:%4.2f}&apikey=%s HTTP/1.1\r\nHost: %s\r\nUser-Agent: %s %s\r\nConnection: keep-alive\r\n\r\n", config->pNodeName, data->Humidity, data->Temperature, data->Pressure, config->pApiKey, config->pHostName, TOOLNAME, BME280_VERSION);
 
     printf ("-----\nbuflen: %ld\n%s\n", strlen(tcp_buffer), tcp_buffer);
     printf ("sent: %ld\n", send(socket_fd, tcp_buffer, strlen(tcp_buffer), 0));
